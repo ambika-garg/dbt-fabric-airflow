d0df6832eb3ab780635f7ed34a2f8dd9cb80412d
ambika-garg <ambikagarg1101.com>
2023-09-27 15:36:50 -0400

    Added version

--- dags/dbt_core.py
+++ dags/dbt_core.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 
 # We're hardcoding the project directory value here for the purpose of the demo, but in a production
 # environment this would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "dbt"
+DBT_PROJECT_DIR = "jaffle_shop\dbt"
 
 with DAG(
     "dbt-core-fabrics",
@@ -31,7 +31,7 @@ with DAG(
     
     dbt_run = BashOperator(
         task_id="dbt_run",
-        bash_command="dbt run",
+        bash_command=f"dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
     )
 
     dbt_version >> dbt_run
