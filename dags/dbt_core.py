92369218c9d1e02c0d62cff9d3ad4f89bb129368
root <root@DESKTOP-C1DQG1B>
2023-09-27 17:40:10 -0400

    Added loggin in dbt

--- dags/dbt_core.py
+++ dags/dbt_core.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 
 # We're hardcoding the project directory value here for the purpose of the demo, but in a production
 # environment this would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "/opt/airflow/git/dbt-fabric-airflow.git/dbt"
+DBT_PROJECT_DIR = "dbt"
 
 with DAG(
     "dbt-core-fabrics",
@@ -36,7 +36,7 @@ with DAG(
     
     dbt_run = BashOperator(
         task_id="dbt_run",
-        bash_command=f"dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+        bash_command=f"dbt --log-format run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR} \ cat dbt.log" ,
     )
 
     dbt_version >> dbt_run
