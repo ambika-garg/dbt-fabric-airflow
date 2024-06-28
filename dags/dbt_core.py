85ab0e9bb32a3d88abd0e01567b1301f03931a25
Ambika Garg <v-ambikagarg@microsoft.com>
2024-06-28 16:28:50 -0400

    Development

--- dags/dbt_core.py
+++ dags/dbt_core.py
@@ -33,7 +33,7 @@ with DAG(
 
     dbt_pwd = BashOperator(
         task_id="dbt_debug",
-        bash_command="cd dbt && dbt debug",
+        bash_command= f"cd {DBT_PROJECT_DIR} && ls -la && pwd",
     )
     
     dbt_run = BashOperator(
